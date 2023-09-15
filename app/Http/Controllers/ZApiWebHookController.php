b10278e4b4a9b49df3c8a038781518d1036c0b47
anderson <andersong.salvador@gmail.com>
2023-09-15 18:45:11 -0300

    att app

--- app/Http/Controllers/ZApiWebHookController.php
+++ app/Http/Controllers/ZApiWebHookController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DialogsQuestion;
+use App\Models\DialogsTemplate;
+use App\Models\GroupQuestionsResponse;
 use App\Models\NeedRequest;
+use App\Models\ResponsesToGroup;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -11,12 +15,14 @@ class ZApiWebHookController extends Controller
 {
     public function getStatusMessage(Request $request)
     {
-        return;
         Log::info("-------WebhoockLaravel---------");
         $dados = $request->all();
 
         //vrificar se o dado recebido de isGroup é falso se sim é uma pessoa enviando mensagem;
         if ($dados['isGroup']) {
+            return;
+        }
+        if ($dados['phone'] != '554195640242') {
             return;
         }
         //se não existir criar um usuario
@@ -34,46 +40,46 @@ class ZApiWebHookController extends Controller
             $user = $newUser;
         }
 
+        
         //verificar se este telefone tem um chamado em aberto
         $nextNedRequest = NeedRequest::where('user_id', $user->id)->where('status_id', '!=', 3)->first();
-
+        
         $zApiController = new ZApiController();
-        // se não existir um pedido;
-        if (!$nextNedRequest && $dados['text']['message'] != '1') {
-            $zApiController->sendMessage($dados['phone'], "Olá seja bem vindo a aplicação e-greja.\n1-precisa de ajuda\n2-não obrigado");
+        //SE NÃO ESTIVER SIDO INICIADO UMA CONVERÇA
+        if (!$nextNedRequest) {
+            $selectTemplateQuestions =  DialogsTemplate::where('title', 'Egreja')->first();
+            $dialogQuestion = DialogsQuestion::where('dialog_template_id', $selectTemplateQuestions->id)->orderBy('priority', 'ASC')->first();
+            
+            $this->createDefaultNeedRequest($user);
+            $zApiController->sendMessage($dados['phone'], str_replace('\n', "\n", $dialogQuestion->question));
             return;
-        } else if (!$nextNedRequest && $dados['text']['message'] == '1') {
-            //cria uma linha em need_reuquests
-            $needrequest = new NeedRequest();
-            $needrequest->user_id = $user->id;
-            $needrequest->status_id = 1;
-            $needrequest->current_dialog_id = 1;
-            $needrequest->save();
-            $zApiController->sendMessage($dados['phone'], "Ok enviamos o seu pedido para os nossos voluntarios logo vc recebera um contato.");
         }
-
-
-        //se existir um pedido
+        //SE JA ESTIVER SIDO INICIADO UMA CONVERÇA
         if ($nextNedRequest) {
-            switch ($nextNedRequest->current_dialog_id) {
-                case 1:
-                    if ($dados['text']['message'] === '1') {
-                        $zApiController->sendMessage($dados['phone'], "Pedido de ajuda cancelado.");
-                        $nextNedRequest->status_id = 3;
-                        $nextNedRequest->save();
-                        return;
-                    }
-                    $zApiController->sendMessage($dados['phone'], "Verificamos que vc possui um peido de ajuda em aberto\n1-Cancelar pedido de ajuda.");
-                    # code...
-                    break;
+            $dialogQuestions = GroupQuestionsResponse::where('dialog_question_id',$nextNedRequest->current_dialog_question_id)->get();
+            $responsesQuestions = ResponsesToGroup::whereIn('group_questions_response_id',$dialogQuestions->pluck('id'))->get();
+            //coloca todas as respostas positivas em um array exemplo;
+            $positiveResponse = $responsesQuestions->pluck('response')->toArray();
 
-                default:
-                    # code...
-                    break;
+            //verifica se a mensagem enviada pelo usuario bate com alguma dessas;
+            if(in_array($dados['text']['message'],$positiveResponse)){
+                Log::info('A mensagem bateu');
+                return;
+            }else{
+                Log::info('não entendi por favor repita');
+                return;
             }
         }
-
-
+        
         return response()->json(['message' => 'Dados do webhook recebidos com sucesso'], 200);
+    }
+
+    public function createDefaultNeedRequest($user)
+    {
+        $needrequest = new NeedRequest();
+        $needrequest->user_id = $user->id;
+        $needrequest->status_id = 1;
+        $needrequest->current_dialog_question_id = 1;
+        $needrequest->save();
     }
 }
