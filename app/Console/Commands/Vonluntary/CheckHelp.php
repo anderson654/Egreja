610f016e70b62120567a2248594c486e57f42aab
anderson <andersong.salvador@gmail.com>
2023-10-20 16:32:49 -0300

    att  app

--- app/Console/Commands/Vonluntary/CheckHelp.php
+++ app/Console/Commands/Vonluntary/CheckHelp.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands\Vonluntary;
 
+use App\Models\PrayerRequest;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -26,7 +28,36 @@ class CheckHelp extends Command
      */
     public function handle()
     {
+        $prayerRequests = PrayerRequest::where('status_id', 1)->has('prayer')->get();
         //
-        Log::info('Hello Word');
+        foreach ($prayerRequests as $prayerRequest) {
+            //caso passe de 30 min e ninguem atendeu fechar o chamado e enviar uma mensagem de desculpa
+            $this->closePrayer30Minuts($prayerRequest);
+
+            //verificar se alguem ja aceitou o chamado e fechar para todos enviando uma mensagem
+            // $this->closePrayer30Minuts($prayerRequest);
+
+            //apos 10 min enviar avaliação.
+        }
     }
+
+    public function closePrayer30Minuts($prayerRequest)
+    {
+        $limitTime = Carbon::parse($prayerRequest->created_at->toString())->addMinutes(30);
+        if ($limitTime < Carbon::now()) {
+            $prayerRequest->status_id = 3;
+            $prayerRequest->update();
+            //pegar todos os pedidos relacionados e fechar
+            $voluntaryPrayerRequests = PrayerRequest::where('reference', $prayerRequest->id)->get();
+            foreach ($voluntaryPrayerRequests as $voluntaryPrayerRequest) {
+                # code...
+                $voluntaryPrayerRequest->status_id = 3;
+                $voluntaryPrayerRequest->update();
+            }
+        }
+    }
+
+    // public function sendAvaliable(){
+
+    // }
 }
