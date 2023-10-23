880a6411d38c61fcfc10ff4322e846b08d64107d
anderson <andersong.salvador@gmail.com>
2023-10-23 18:01:08 -0300

    att app

--- app/Http/Controllers/ZApiWebHookController.php
+++ app/Http/Controllers/ZApiWebHookController.php
@@ -234,7 +234,7 @@ class ZApiWebHookController extends Controller
                     sleep(1);
                     $existPrayerRequest = VolunteerRequest::where('user_id', $obj['id'])->where('status_id', 1)->exists();
 
-                    if ($originalPhone === "5541985071326") {
+                    // if ($originalPhone === "5541985071326") {
                         if (!$existPrayerRequest) {
                             $user = User::find($obj['id']);
                             $selectTemplateQuestions =  DialogsTemplate::where('title', 'Egreja-Voluntary')->first();
@@ -242,7 +242,7 @@ class ZApiWebHookController extends Controller
                             $this->createDefaultPrayerRequest($user, $dialogQuestion->id, $nextNedRequest->id);
                             $zApiController->sendMessage($originalPhone, str_replace('\n', "\n", $dialogQuestion->question));
                         }
-                    }
+                    // }
                 }
 
                 break;
