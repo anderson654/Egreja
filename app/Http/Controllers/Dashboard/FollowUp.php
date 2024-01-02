589c97297b589f7ac7a66e0ebe48bc439850b74c
anderson <andersong.salvador@gmail.com>
2024-01-02 18:03:14 -0300

    att app

--- app/Http/Controllers/Dashboard/FollowUp.php
+++ app/Http/Controllers/Dashboard/FollowUp.php
@@ -14,7 +14,7 @@ class FollowUp extends Controller
     public function index()
     {
         //
-        $sideDidhes = SideDishes::get();
+        $sideDidhes = SideDishes::orderBy('created_at', 'desc')->get();
         return view('pages.followUp.index',compact('sideDidhes'));
     }
 
