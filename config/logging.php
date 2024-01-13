c51ddb615df45ecd94185a5526498ba4b4b6c913
anderson <andersong.salvador@gmail.com>
2024-01-13 11:47:16 -0300

    att app

--- config/logging.php
+++ config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -129,7 +129,8 @@ return [
         'notify_prayer_request' => [
             'driver' => 'daily',
             'path' => storage_path('logs/notify-prayer-request.log'),
-            'level' => 'info'
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true
         ]
     ],
 
